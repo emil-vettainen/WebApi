6f19db6c4fbba89c94f61a23d32d209ad7f4d43d
emil.vettainen <emil.vettainen@utb.ecutbildning.se>
2024-04-19 01:50:08 +0200

    2024-04-19-01-50

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,25 +1,12 @@
 using Business.AutoMapper;
-//using Business.Services;
-//using Infrastructure.Contexts;
-//using Infrastructure.Repositories.ContactRepositories;
-//using Infrastructure.Repositories.CoursesRepositories;
-//using Infrastructure.Repositories.Subscribers;
-//using Microsoft.EntityFrameworkCore;
 using WebApi.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
-
-//builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer")));
-
-
-//builder.Services.AddDbContext<CosmosDbContext>(x => x.UseCosmos("AccountEndpoint=https://evettainen.documents.azure.com:443/;AccountKey=p7pfxnLGAF3LNixPDdrcXuz58iuWdD0tlDf9YLJswUP530PVVlWTL1MMsz7xML98cWXFghwwhCdBACDbOowCEQ==;", "WebApi"));
-
 
 builder.Services.RegisterDbContext(builder.Configuration);
 builder.Services.RegisterServices();
